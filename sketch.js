2dbc5422e8498ce833098c36915b7ada465f7a19
painkillerkid <91149449+painkillerkid@users.noreply.github.com>
2023-06-19 12:17:17 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ let clicked = false
 
 function preload(){
     // audio = createAudio("Katy Perry - Firework (Acoustic Session) REMASTERED.mp3");
-    audio = createAudio("Carly Rae Jepsen - Call Me Maybe (Audio) [TubeRipper.com].m4a");
+    audio = createAudio("50 Cent - In Da Club (Lyrics) [TubeRipper.com].mp3");
 
 imgs[0]= loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687153640/LongSleeveShirt_Trial1_u2hlx2.png')
  imgs[1] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687167050/LongSleeveShirt_Trial2_tkob7b.png') 
