945d83d1d42039abe43c62e53d8eae73cfde3c2c
Arthur Dean <91149449+disneylandsucks@users.noreply.github.com>
2023-06-20 18:33:02 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -71,7 +71,7 @@ imgs[0]= loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v16871
                 imgs[37] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687310653/LongSleeveShirt_Trial37_diub2f.png')
 
 
-                // imgs[38] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687307077/LongSleeveShirt_Trial33_psiumz.png')
+                imgs[38] = loadImage('https://res.cloudinary.com/painkillerkid/image/upload/v1687311136/LongSleeveShirt_Trial38_jakvlj.png')
 
 
   mod = loadModel('THEREALLONGSLEEVESHIRT.obj')
